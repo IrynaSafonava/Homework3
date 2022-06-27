16329f10b4d4f7d108e2b577d06d39e98ee0dc5d
iryna_s <emily_89@mail.ru>
2022-06-28 00:44:25 +0300

    fix: sql added

--- src/main/resources/example.sql
+++ src/main/resources/example.sql
@@ -7,7 +7,7 @@ address VARCHAR (255)
 CREATE TABLE Accounts (
 accountId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 userId INTEGER (10) NOT NULL,
-balance DECIMAL (15),
+balance DECIMAL (15, 3),
 currency VARCHAR (10) NOT NULL,
 FOREIGN KEY (userId) REFERENCES Users(userID),
 UNIQUE (userId, currency)
@@ -16,6 +16,6 @@ UNIQUE (userId, currency)
 CREATE TABLE Transactions (
 transactionId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 accountId INTEGER (10) NOT NULL,
-amount DECIMAL (15) NOT NULL,
+amount DECIMAL (15, 3) NOT NULL,
 FOREIGN KEY (accountId) REFERENCES Accounts(accountId)
 );
